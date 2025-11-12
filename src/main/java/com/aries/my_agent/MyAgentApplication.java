a9b2e683c20b3e37639d59316389160c80dfa610
Aries <arieswu0324@gmail.com>
2025-11-12 15:07:47 +0800

    init

---
+++ src/main/java/com/aries/my_agent/MyAgentApplication.java
@@ -0,0 +1,13 @@
+package com.aries.my_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aries/my_agent/MyAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aries.my_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
